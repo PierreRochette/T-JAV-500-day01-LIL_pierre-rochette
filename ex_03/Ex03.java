dab0b14997dc6e2815c20032343044313c969b26
Pierre Rochette <pierre.rochette@epitech.eu>
2023-10-23 11:21:02 +0200

    task 3

---
+++ ex_03/Ex03.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Ex03 {
+
+    public static void printArray(ArrayList<String> myArray) {
+
+        for (String element : myArray) {
+            System.out.println(element);
+        }
+
+    }
+
+        }
